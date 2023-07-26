e18b6ab9a6faac03083ffccf4f05d51ed94d0ac1
Blessing Udeji <budeji@gmail.com>
2023-07-26 02:37:15 +0100

    Commit for bubble sort

---
+++ sort.h
@@ -0,0 +1,42 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+/**
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+int partition(int *array, size_t size, int left, int right);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void q_sort(int *array, size_t size, int left, int right);
+void swap(int *a, int *b);
+void swap_back_node(listint_t **list, listint_t **tail, listint_t **curr);
+void swap_front_node(listint_t **list, listint_t **tail, listint_t **curr);
+void max_heap(int *array, size_t size, size_t first, size_t index);
+
+#endif
